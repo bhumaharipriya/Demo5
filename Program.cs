2268aee974824b4687fc269231319ae15cc40f5d
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 16:26:10 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Demo5
     {
         static void Main(string[] args)
         {
-            
+              int a = 10;
+            int? b = null;
+            int? c = 100;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int k = b?? 0;
+            Console.WriteLine(k);
+            int y = c ?? 0;
+            Console.WriteLine(y);
         }
     }
 }
